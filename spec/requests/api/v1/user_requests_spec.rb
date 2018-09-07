47e829e1a500b8c3413080acc81f8c7f6a61dcb7
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-06 18:11:19 -0600

    TESTS Users#create action

---
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'User Requests' do
+  context 'POST /api/v1/users' do
+    it 'should create a user and return a 204 if successful' do
+      body = { uid: 123456 }.to_json
+
+      post '/api/v1/users', params: body
+
+      expect(response.status).to eq(204)
+    end
+
+    it 'should return a 400 if something went wrong' do
+      errant_body = { uid: '' }.to_json
+
+      post '/api/v1/users', params: errant_body
+
+      response_data = JSON.parse(response.body)
+      expected_response = { error: 'User creation unsuccessful.'}
+
+      expect(response.status).to eq(400)
+      expect(response_data).to eq(expected_response)
+    end
+  end
+end

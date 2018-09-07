f5f0a9dfbd867f15f2be907b609911d111a0792f
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-07 09:35:52 -0600

    TESTS new User attributes of email, username, and profile_pic_url

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'Validations' do
-    it { should validate_presence_of(:uid) } 
+    it { should validate_presence_of(:uid) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should have_db_column(:profile_pic_url) }
   end
 end

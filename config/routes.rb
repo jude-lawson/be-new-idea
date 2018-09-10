70eceb6e11da8125da0cbcdba8915e9684b76651
zdcthomas <zdcthomas@yahoo.com>
2018-09-10 15:48:15 -0600

    implements get WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # Ideas
+      resources :ideas, only: [:index, :show]
+      
       # Users
       get '/users/:id', to: 'users#show' 
       post '/users', to: 'users#create'
-
-      # Ideas
-      get '/ideas', to: 'ideas#index'
 
       # Contributions
       post '/ideas/:id/contributions', to: 'contributions#create'

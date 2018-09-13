9f0959b990222d76a0f12b0488d8ae4c9814dcc5
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-13 12:11:12 -0600

    Updates syntaxt in test for Ideas#edit

--- spec/requests/api/v1/ideas/edit_idea_requests_spec.rb
+++ spec/requests/api/v1/ideas/edit_idea_requests_spec.rb
@@ -5,20 +5,21 @@ RSpec.describe 'Edit Idea Requests' do
     setup_users_and_posts
   end
 
-  decsribe 'PATCH /api/v1/ideas/:id' do
+  describe 'PATCH /api/v1/ideas/:id' do
     it 'should edit the idea and return a 201 if successful' do
-      new_idea_content = { title: 'A new title', body: 'This is some new content for the idea' }
+      edited_idea_content = { title: 'A new title', body: 'This is some new content for the idea' }
       edited_idea_id = @user1_ideas[0].id
   
-      patch "/api/v1/ideas/#{edited_idea_id}", params: new_idea_content.to_json
+      patch "/api/v1/ideas/#{edited_idea_id}", params: edited_idea_content.to_json
 
       expect(response.status).to eq(201)
-      expect(Idea.find(@user1_ideas[0].id).body).to eq(new_idea_content[:body])
-      expect(Idea.find(@user1_ideas[0].id).title).to eq(new_idea_content[:title])
+      expect(Idea.find(@user1_ideas[0].id).body).to eq(edited_idea_content[:body])
+      expect(Idea.find(@user1_ideas[0].id).title).to eq(edited_idea_content[:title])
     end
   
     it 'should return a 400 error with standard error message if unsuccessful' do
-      patch "/api/v1/ideas/999", params: new_idea_content.to_json
+      edited_idea_content = { title: 'A new title', body: 'This is some new content for the idea' }
+      patch "/api/v1/ideas/999", params: edited_idea_content.to_json
 
       feedback = JSON>parse(response.body)
 

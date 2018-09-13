43332c7481009f1acd4e3c4d60c9b50048b06604
zdcthomas <zdcthomas@yahoo.com>
2018-09-13 12:10:04 -0600

    tests post idea

---
+++ spec/requests/api/v1/ideas/post_idea_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'Idea Creation' do
+  describe 'post api/v1/ideas' do
+    it 'should create a new idea with viable params' do
+      username = 'my_username'
+      uid = "abc123"
+      email = "email@place.com"
+      profile_pic_url = "www.image.com"
+      user = User.create!(uid:uid , email:email, username:username, profile_pic_url:profile_pic_url)
+
+      title = "My big new idea"
+      body = "This is the body"
+      new_idea_params = {
+        user_id: user.id,
+        title: title,
+        body: body
+      }
+
+      post '/api/v1/ideas', params: new_idea_params
+      
+      returned_resp = JSON.parse(response.body)
+      idea_in_db = Idea.find_by_title(title)
+      expect(idea_in_db).to exist
+      expect(idea_in_db.body).to eq(body)
+      expect(respone.status).to eq(201)
+      expect(returned_resp['message']).to eq('Idea successfully created')
+    end
+    it 'should not create the idea with missing fields' do
+      username = 'my_username'
+      uid = "abc123"
+      email = "email@place.com"
+      profile_pic_url = "www.image.com"
+      user = User.create!(uid:uid , email:email, username:username, profile_pic_url:profile_pic_url)
+
+      body = "This is the body"
+      new_idea_params = {
+        user_id: user.id,
+        body: body
+      }
+
+      post '/api/v1/ideas', params: new_idea_params
+      
+      returned_resp = JSON.parse(response.body)
+      idea_in_db = Idea.find_by_title(title)
+      expect(idea_in_db).to_not exist
+      expect(respone.status).to eq(400)
+      expect(returned_resp['message']).to eq('An error has occurred')
+    end
+  end
+end

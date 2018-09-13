a4c969c702fd7dc1ff7d5d4deeae67c04d130cf2
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-13 12:09:30 -0600

    Tests Ideas#edit

---
+++ spec/requests/api/v1/ideas/edit_idea_requests_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit Idea Requests' do
+  before :each do
+    setup_users_and_posts
+  end
+
+  decsribe 'PATCH /api/v1/ideas/:id' do
+    it 'should edit the idea and return a 201 if successful' do
+      new_idea_content = { title: 'A new title', body: 'This is some new content for the idea' }
+      edited_idea_id = @user1_ideas[0].id
+  
+      patch "/api/v1/ideas/#{edited_idea_id}", params: new_idea_content.to_json
+
+      expect(response.status).to eq(201)
+      expect(Idea.find(@user1_ideas[0].id).body).to eq(new_idea_content[:body])
+      expect(Idea.find(@user1_ideas[0].id).title).to eq(new_idea_content[:title])
+    end
+  
+    it 'should return a 400 error with standard error message if unsuccessful' do
+      patch "/api/v1/ideas/999", params: new_idea_content.to_json
+
+      feedback = JSON>parse(response.body)
+
+      expect(response.status).to eq(400)
+      expect(response['message']).to eq('An error has occurred')
+      expect(response['error']).to include('ActiveRecord::RecordNotFound')
+    end
+  end
+end

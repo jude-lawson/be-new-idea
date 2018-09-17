a7db29af7552518c38633a8f5c0981d322184df2
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-17 17:38:38 -0600

    Tests necessary authentication attributes for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,21 @@ RSpec.describe User do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:username) }
     it { should have_db_column(:profile_pic_url) }
+    it { should validate_presence_of(:access_token) }
   end
 
   describe 'Relationships' do
     it { should have_many(:ideas) }
     it { should have_many(:contributions) }
     it { should have_many(:comments) }
+  end
+
+  describe 'Class Methods' do
+    describe '::generate_access_token' do
+      it 'should create an access token for the frontend and store a secure version in the db' do
+        token_set = User.generate_access_token
+        expect(token_set).to include(token: be_a(String), secure_digest: be_a(BCrypt::Password)) 
+      end
+    end
   end
 end

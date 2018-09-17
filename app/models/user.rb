023d74ee410ee22d660adad0539d2f0c725eee1a
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-17 17:40:19 -0600

    Adds access_token db column for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-  validates_presence_of :uid, :email, :username
+  validates_presence_of :uid, :email, :username, :access_token
 
   has_many :ideas
   has_many :contributions
   has_many :comments
+
+  def self.generate_access_token
+    token = SecureRandom.uuid
+    secure_digest = BCrypt::Password.new(BCrypt::Password.create(token))
+    { token: token, secure_digest: secure_digest }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_153631) do
+ActiveRecord::Schema.define(version: 2018_09_17_233915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_09_07_153631) do
     t.string "email"
     t.string "username"
     t.string "profile_pic_url"
+    t.string "access_token"
   end
 
   add_foreign_key "comments", "contributions"

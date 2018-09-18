9b5745a86830d531d0b23e4ee7cc8395628cf3f0
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-18 11:22:05 -0600

    Implements gateway for Comments#create and Comments#edit and updates appropriate tests

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::CommentsController < ApplicationController
   def edit
-    safe_query do
+    gateway do
       Comment.find(params[:id]).update(parsed_response)
       render status: 201
     end
   end
   
   def create
-    safe_query do
+    gateway do
       contribution = Contribution.find(params[:id])
       contribution.comments.create!(parsed_response)
       render status: 201

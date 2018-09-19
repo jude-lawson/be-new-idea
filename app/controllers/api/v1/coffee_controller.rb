e7d405d251e1d898ff0f32b603ab08cb7c9cec22
zdcthomas <zdcthomas@yahoo.com>
2018-09-19 13:37:54 -0600

    detecting coffee params

--- app/controllers/api/v1/coffee_controller.rb
+++ app/controllers/api/v1/coffee_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::CoffeeController < ApplicationController
   def brew
-    render json: {message: "I'm a Teapot!"}, status: 418
+    safe_query do
+      bev = params['beverage']
+      case bev
+      when 'tea'
+        render json: {message: "Here's some tea"}, status: 200
+      else
+        render json: {message: "I'm a Teapot!"}, status: 418 
+      end
+    end
   end
 end
